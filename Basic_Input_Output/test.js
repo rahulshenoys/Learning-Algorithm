7ee4d664c4b63fce4c9b60eb43f72457bda11e75
rahulshenoy1 <rahul.shenoy@exathought.com>
2024-05-07 22:36:39 +0530

    Making Develop as Main Branch

---
+++ Basic_Input_Output/test.js
@@ -0,0 +1,17 @@
+// console.log("hello Rahul");
+// console.log("hello Rahul\n");
+// return 0;
+
+// const readline = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// readline.question("Enter a number: ", (userInput) => {
+//     // Convert the input to a number and continue processing
+//     let number = parseInt(userInput);
+//     // ... your logic here
+//     console.log("the number is:", number);
+//     readline.close();
+// });
+
